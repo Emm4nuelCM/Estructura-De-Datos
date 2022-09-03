3792e4af4f7d75f34699e908b30d6f5fa779f6a5
Emmanuel Camacho <ecamacho5@ucol.mx>
2022-09-02 21:51:59 -0500

    Coladera de Eratostenes

---
+++ coladeradeeratostenes.js
@@ -0,0 +1,24 @@
+let iteraciones=0;
+let valores = [];
+let primos = [];
+let maxDivisor = Math.floor(Math.sqrt(1000));
+
+for(let i=0;i<1000-1;i++) {
+    valores.push(true);
+}
+
+for(let i = 2;i<=maxDivisor;i++) {
+    if(valores[i-2]) {
+        for(let j = i;j<=Math.floor(1000/i);j++) {
+            valores[(i*j)-2]=false;
+            iteraciones++;
+        }
+    }
+    iteraciones++;
+}
+
+valores.forEach((elements, index)=> {
+    if(elements) primos.push(index+2);
+});
+
+console.log(`Los numeros primos son: ${primos}`);
