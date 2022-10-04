4b4e75311234516ba52d5ce28c963fa5ce367f0f
Emmanuel Camacho <ecamacho5@ucol.mx>
2022-10-04 11:52:34 -0500

    Classes in the same script.js

--- ControlDeInventario/script.js
+++ ControlDeInventario/script.js
@@ -61,3 +61,16 @@ class Inventory{
         }
     }
 }
+
+class Product{
+    constructor(code, name, amount, cost) {
+        this.code = code;
+        this.name = name;
+        this.amount = amount;
+        this.cost = cost;
+    }
+
+    info() {
+        return `Codigo: ${this.code} Nombre: ${this.name} Cantidad: ${this.amount} Costo: ${this.cost}`
+    }   
+}
